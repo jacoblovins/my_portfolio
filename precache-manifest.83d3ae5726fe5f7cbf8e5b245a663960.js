1a214256bcbd6e92a15f33d20bb56b324d582e6b
jacoblovins <lovins.jacob@yahoo.com>
2020-10-31 17:38:27 -0400

    Updates

---
+++ precache-manifest.83d3ae5726fe5f7cbf8e5b245a663960.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f828b27e08662e61e9cb288d6724eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5d91c4427317a7cb67e",
+    "url": "/static/css/main.bbd5451b.chunk.css"
+  },
+  {
+    "revision": "af451cd77faf0c286655",
+    "url": "/static/js/2.a48c610a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a48c610a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d91c4427317a7cb67e",
+    "url": "/static/js/main.b6240af3.chunk.js"
+  },
+  {
+    "revision": "5355aeeb1e20cb577cce",
+    "url": "/static/js/runtime-main.e7bd7d91.js"
+  },
+  {
+    "revision": "18632baf1aa4b87098bf0b11315b07c7",
+    "url": "/static/media/leather.18632baf.jpg"
+  },
+  {
+    "revision": "f399e0b5a88b70b9ac8fe25400509185",
+    "url": "/static/media/plate.f399e0b5.jpeg"
+  }
+]);

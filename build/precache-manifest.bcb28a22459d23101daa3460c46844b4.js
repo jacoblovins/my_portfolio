627749d19f04ac56bf17a6e8c1c10bae671dd613
jacoblovins <lovins.jacob@yahoo.com>
2020-11-13 10:29:54 -0500

    readme

---
+++ build/precache-manifest.bcb28a22459d23101daa3460c46844b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01337f7e37eefa7afa6963b93d6386a9",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "f46fa99548e4b4f08c27",
+    "url": "/my_portfolio/static/css/main.a5da2fa7.chunk.css"
+  },
+  {
+    "revision": "ff825e68bf429af27b45",
+    "url": "/my_portfolio/static/js/2.5a396b5f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my_portfolio/static/js/2.5a396b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46fa99548e4b4f08c27",
+    "url": "/my_portfolio/static/js/main.e22bfe48.chunk.js"
+  },
+  {
+    "revision": "d9629bd1ee9955b69a66",
+    "url": "/my_portfolio/static/js/runtime-main.88b1f98b.js"
+  },
+  {
+    "revision": "18632baf1aa4b87098bf0b11315b07c7",
+    "url": "/my_portfolio/static/media/leather.18632baf.jpg"
+  },
+  {
+    "revision": "f399e0b5a88b70b9ac8fe25400509185",
+    "url": "/my_portfolio/static/media/plate.f399e0b5.jpeg"
+  }
+]);

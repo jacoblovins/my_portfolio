d00f3c2f3ec6708d55696295fb4746ef5db7c433
jacoblovins <lovins.jacob@yahoo.com>
2020-11-13 13:24:36 -0500

    another pages try

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './css/app.css';
 import Navbar from './components/Navbar/Navbar';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route, HashRouter as Router, Switch } from 'react-router-dom'
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';

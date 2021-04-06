bcdaabe9ad03cb4817d5da5a5be53fe39258b1b1
jacoblovins <lovins.jacob@yahoo.com>
2021-04-06 09:37:22 -0400

    sent new files to build

---
+++ build/precache-manifest.1d3406a540072e59ad3b8e5264923cde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d71bd41aa2ae5111ac258a1d818037",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "b1b5cde8eafc8b63ef8a",
+    "url": "/my_portfolio/static/css/main.4f5953b6.chunk.css"
+  },
+  {
+    "revision": "d472c1e5b05cec049823",
+    "url": "/my_portfolio/static/js/2.a5ccc071.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my_portfolio/static/js/2.a5ccc071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b5cde8eafc8b63ef8a",
+    "url": "/my_portfolio/static/js/main.1dd0d41e.chunk.js"
+  },
+  {
+    "revision": "d9629bd1ee9955b69a66",
+    "url": "/my_portfolio/static/js/runtime-main.88b1f98b.js"
+  },
+  {
+    "revision": "18632baf1aa4b87098bf0b11315b07c7",
+    "url": "/my_portfolio/static/media/leather.18632baf.jpg"
+  },
+  {
+    "revision": "f399e0b5a88b70b9ac8fe25400509185",
+    "url": "/my_portfolio/static/media/plate.f399e0b5.jpeg"
+  }
+]);

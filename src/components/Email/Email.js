dd06713ff7f0e385b9f9e018a47e5d30325cc14b
jacoblovins <lovins.jacob@yahoo.com>
2021-04-06 09:26:17 -0400

    user notified when mesesage is sent

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -8,6 +8,7 @@ function Email() {
     const [name, setName] = useState();
     const [email, setEmail] = useState();
     const [message, setMessage] = useState();
+    const [sent, setSent] = useState(false)
 
     const handleNameChange = event => {
         setName(event.target.value);
@@ -22,33 +23,44 @@ function Email() {
 
     function sendEmail(e) {
         e.preventDefault();
-    
+
         emailjs.sendForm('service_mq4savi', 'template_lzoegri', e.target, 'user_nwATB7engk4AXJIy90rCp')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-      }
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        setSent(true)
+    }
 
 
 
     return (
-        <form onSubmit={sendEmail}>
-            <div className="form-group">
-                <label>Name:</label>
-                <input type="text" className="form-control" id="usr" name="name" value={name} onChange={handleNameChange} placeholder="John Smith" />
-            </div>
-            <div className="form-group">
-                <label>Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" name="email" value={email} onChange={handleEmailChange} placeholder="example@email.com" />
-            </div>
-            <div className="form-group">
-                <label >Message</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" name="message" value={message} onChange={handleMessageChange} rows="3"></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        <div>
+            {sent === false ? (
+                <form onSubmit={sendEmail}>
+                    <div className="form-group">
+                        <label>Name:</label>
+                        <input type="text" className="form-control" id="usr" name="name" value={name} onChange={handleNameChange} placeholder="John Smith" />
+                    </div>
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" name="email" value={email} onChange={handleEmailChange} placeholder="example@email.com" />
+                    </div>
+                    <div className="form-group">
+                        <label >Message</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" name="message" value={message} onChange={handleMessageChange} rows="3"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            ) : (
+                    <div>
+                    <h2>Message Sent!</h2>
+                    </div>
+                )}
+        </div>
+
     )
 }
 
